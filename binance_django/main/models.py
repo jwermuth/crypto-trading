7dede79e6082175e8db30ac48c0326062e4b59ef
Jesper Wermuth <jwermuth@gmail.com>
2021-01-20 17:51:24 +0100

    models for get_account

--- binance_django/main/models.py
+++ binance_django/main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Account(models.Model):
+    pass
+
+
+class Balance(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    asset = models.CharField(max_length=10)
+    free = models.FloatField()
+    locked = models.FloatField()
