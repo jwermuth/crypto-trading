9635451218a6fe72f6d5c38ff55f7363f034e568
Jesper Wermuth <jwermuth@gmail.com>
2021-01-21 13:57:14 +0100

    easy to construct model object from dict returned by binance

--- binance_django/main/models.py
+++ binance_django/main/models.py
@@ -1,13 +1,31 @@
+import typing
 from django.db import models
 
 
-# Create your models here.
 class Account(models.Model):
-    pass
+    makerCommission = models.FloatField(null=True)
+    takerCommission = models.FloatField(null=True)
+    buyerCommission = models.FloatField(null=True)
+    sellerCommission = models.FloatField(null=True)
+    canTrade = models.BooleanField(null=True)
+    canWithdraw = models.BooleanField(null=True)
+    canDeposit = models.BooleanField(null=True)
+    updateTime = models.TimeField(null=True)
+    accountType = models.CharField(max_length=20, null=True)
+
+    def __init__(self, *args, response: typing.Any=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if response is not None:
+            self.__dict__.update(response)
 
 
 class Balance(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='balances')
     asset = models.CharField(max_length=10)
     free = models.FloatField()
     locked = models.FloatField()
+
+    def __init__(self, *args, response: typing.Any=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if response is not None:
+            self.__dict__.update(response)
--- binance_django/main/views.py
+++ binance_django/main/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from pprint import pformat
 from .binance_helpers import BinanceClient, TradePairs
 from django.template import loader
+from .models import Balance, Account
+#from collections import namedtuple
 
 
 def index(request):
@@ -15,5 +17,15 @@ def get_account(request):
 
     template = loader.get_template('main/get_account.html')
     account = client.get_account()
+    print(account)
+    #account = namedtuple("Account", account.keys())(*account.values())
+    account = Account(account)
+    print(type(account))
+    print(account)
+    print(type(account.balances.all()))
+    for b in account.balances.all():
+        print(type(b))
+        print(b)
+
     context = {'json_response': pformat(account), 'account_info': account}
     return HttpResponse(template.render(context, request))
