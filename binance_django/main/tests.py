82578d2666f56c90fc7a08c892e8ac0e0013b861
Jesper Wermuth <jwermuth@gmail.com>
2021-01-23 12:24:18 +0100

    basic test

--- binance_django/main/tests.py
+++ binance_django/main/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class MainIndexViewTests(TestCase):
+    def test_is_present(self):
+        """
+        Something responds at the index url
+        """
+        response = self.client.get(reverse('main:index'))
+        self.assertEqual(response.status_code, 200)
